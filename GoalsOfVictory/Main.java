5dae40ca916857a3de04c4a8d6843911fc5e8c00
Sarika Mehra <mehrasarika574@gmail.com>
2026-07-27 11:39:39 +0530

    1877A is added

---
+++ GoalsOfVictory/Main.java
@@ -0,0 +1,24 @@
+package GoalsOfVictory;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+
+            int sum = 0 ;
+
+            for(int i = 0 ; i < n - 1; i++){
+               sum += sc.nextInt();
+            } 
+        
+            System.out.println(-sum); // sum + x = 0 
+            // so, x = -sum
+        }
+    }
+}
